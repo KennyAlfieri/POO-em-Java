0180e037608712760cd6208c3cb6e0c6405c5144
Kenny Alfieri <kenny.v.alfieri@gmail.com>
2026-05-07 08:29:38 -0300

    aplicação de polimorfismo

---
+++ src/Aula11/ponto/Ponto.java
@@ -0,0 +1,33 @@
+
+package Aula11.ponto;
+
+public class Ponto {
+    private int coordenadaX;
+    private int coordenadaY;
+
+    public Ponto(int coordenadaX, int coordenadaY) {
+        this.coordenadaX = coordenadaX;
+        this.coordenadaY = coordenadaY;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + coordenadaX + ", " + coordenadaY + ")";
+    }
+
+    public int getCoordenadaX() {
+        return coordenadaX;
+    }
+
+    public void setCoordenadaX(int coordenadaX) {
+        this.coordenadaX = coordenadaX;
+    }
+
+    public int getCoordenadaY() {
+        return coordenadaY;
+    }
+
+    public void setCoordenadaY(int coordenadaY) {
+        this.coordenadaY = coordenadaY;
+    }
+}
