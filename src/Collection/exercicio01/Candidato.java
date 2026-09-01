96cbc86a84af5f5a91f1cfae8df4104d8491b360
Kenny Alfieri <kenny.v.alfieri@gmail.com>
2026-09-01 11:30:29 -0300

    Sort List

---
+++ src/Collection/exercicio01/Candidato.java
@@ -0,0 +1,40 @@
+package Collection.exercicio01;
+
+public class Candidato {
+    private String nome;
+    private double notaTecnica;
+    private int anosExperiencia;
+
+    public Candidato(String nome, double notaTecnica, int anosExperiencia) {
+        this.nome = nome;
+        this.notaTecnica = notaTecnica;
+        this.anosExperiencia = anosExperiencia;
+    }
+    public String toString(){
+        return nome+ "\t\t" + notaTecnica + "\t\t" + anosExperiencia;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getNotaTecnica() {
+        return notaTecnica;
+    }
+
+    public void setNotaTecnica(double notaTecnica) {
+        this.notaTecnica = notaTecnica;
+    }
+
+    public int getAnosExperiencia() {
+        return anosExperiencia;
+    }
+
+    public void setAnosExperiencia(int anosExperiencia) {
+        this.anosExperiencia = anosExperiencia;
+    }
+}
---
+++ src/Collection/exercicio01/main.java
@@ -0,0 +1,22 @@
+package Collection.exercicio01;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class main {
+    static void main() {
+        List<Candidato> lista =  new ArrayList<>();
+        lista.add(new Candidato("Patricia",9,30));
+        lista.add(new Candidato("SelminiDroid",7,30));
+        lista.add(new Candidato ("Pedro",6,8));
+        lista.add(new Candidato ("Carlos",8,7));
+
+        //ordenação
+        lista.sort(Comparator.comparingDouble(Candidato::getNotaTecnica).thenComparingInt(Candidato::getAnosExperiencia).reversed().thenComparingDouble(Candidato::getNotaTecnica));
+
+        lista.forEach(candidato -> {
+            System.out.println(candidato);
+        });
+    }
+}
